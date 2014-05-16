080f484b0ffc5b676c6e2521699269444a97b0b6
Jason <jtolhurst01@aol.com>
2014-05-16 11:42:38 -0400

    initial commit
    yeah

---
+++ StrifeLCD.cpp
@@ -0,0 +1,199 @@
+// StrifeLCD.cpp : Defines the entry point for the console application.
+//
+
+#include "StrifeLCD.h"
+#include <stdio.h>
+
+TCHAR monoImageFileName[] = _T("LogiLogoMono.bmp");
+
+StrifeLCD::StrifeLCD(){}
+
+bool StrifeLCD::initStrifeLCD()
+{
+	gold = STARTING_GOLD;
+	gpm = STARTING_GPM;
+	kills = STARTING_KILLS;
+	deaths = STARTING_DEATHS;
+	assists = STARTING_ASSISTS;
+	level = STARTING_LEVEL;
+
+	currentScreen = 0;
+	printf("Initialized\n");
+
+	if (LogiLcdIsConnected(LOGI_LCD_TYPE_MONO))
+	{
+		if (!LogiLcdInit(STRIFELCD_APP_NAME, LOGI_LCD_TYPE_MONO))
+		{
+			return false;
+		}
+		printf("Connected To Keyboard\n");
+		displaySplashScreen();
+		return true;
+	}
+	printf("Not Connected To Keyboard\n");
+	return false;
+}
+
+void StrifeLCD::clearDisplay()
+{
+	BYTE pixels[LOGI_LCD_MONO_WIDTH * LOGI_LCD_MONO_HEIGHT];
+	for (int i = 0; i < (LOGI_LCD_MONO_WIDTH * LOGI_LCD_MONO_HEIGHT); i++)
+	{
+		pixels[i] = (BYTE)0;
+	}
+	LogiLcdMonoSetBackground(pixels);
+	LogiLcdMonoSetText(0, L"");
+	LogiLcdMonoSetText(1, L"");
+	LogiLcdMonoSetText(2, L"");
+	LogiLcdMonoSetText(3, L"");
+}
+
+void StrifeLCD::displaySplashScreen()
+{
+	clearDisplay();
+	printf("I displayed the splash screen\n");
+	setMonoBackgroundFromFile(monoImageFileName);
+	LogiLcdUpdate();
+}
+
+void StrifeLCD::displayStatsScreen1()
+{
+	clearDisplay();
+
+	LogiLcdMonoSetText(0, L"Stats about GPM , Kills");
+	LogiLcdMonoSetText(1, L"Stats about Gold , Deaths");
+	LogiLcdMonoSetText(2, L"Stats about Level , Assists");
+	LogiLcdMonoSetText(3, L"More stats.");
+}
+
+void StrifeLCD::displayStatsScreen2()
+{
+	clearDisplay();
+
+	LogiLcdMonoSetText(0, L"Health");
+	LogiLcdMonoSetText(1, L"this is where a health bar would be.");
+	LogiLcdMonoSetText(2, L"Mana");
+	LogiLcdMonoSetText(3, L"this is where a mana bar would be.");
+}
+
+void StrifeLCD::updateScreen()
+{
+	if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_0))
+	{
+		currentScreen = 0;
+	}
+
+	if (LogiLcdIsButtonPressed(LOGI_LCD_MONO_BUTTON_1))
+	{
+		currentScreen = 1;
+	}
+
+	switch (currentScreen)
+	{
+	case 0:
+		displayStatsScreen1();
+		break;
+	case 1:
+		displayStatsScreen2();
+		break;
+	default:
+		break;
+	}
+
+	LogiLcdUpdate();
+}
+
+bool StrifeLCD::setMonoBackgroundFromFile(TCHAR* fileName)
+{
+	HDC hdc = ::GetDC(NULL);
+
+	Gdiplus::Bitmap* imageMono = Gdiplus::Bitmap::FromFile(fileName);
+
+	if (NULL == imageMono)
+		return false;
+
+	// Get the bitmap handle
+	HBITMAP hBitmap = NULL;
+	Gdiplus::Status status = imageMono->GetHBITMAP(RGB(0, 0, 0), &hBitmap);
+
+	if (status != Gdiplus::Ok)
+		return false;
+
+	BITMAPINFO bitmapInfoMono = { 0 };
+	bitmapInfoMono.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+
+	// Check what we got
+	int ret = GetDIBits(hdc, hBitmap, 0,
+		0,
+		NULL,
+		&bitmapInfoMono, DIB_RGB_COLORS);
+
+	if (LOGI_LCD_MONO_WIDTH != bitmapInfoMono.bmiHeader.biWidth || LOGI_LCD_MONO_HEIGHT != bitmapInfoMono.bmiHeader.biHeight)
+	{
+		::MessageBoxW(NULL, _T("Oooops. Make sure to use a 160 by 43 image for mono background."), _T("LCDDemo"), MB_ICONEXCLAMATION);
+		return false;;
+	}
+
+	bitmapInfoMono.bmiHeader.biCompression = BI_RGB;
+	bitmapInfoMono.bmiHeader.biHeight = -bitmapInfoMono.bmiHeader.biHeight; // this value needs to be inverted, or else image will show up upside/down
+
+	BYTE byteBitmapMono[LOGI_LCD_MONO_WIDTH * LOGI_LCD_MONO_HEIGHT * 4];
+
+	// Gets the "bits" from the bitmap and copies them into a buffer 
+	// which is pointed to by byteBitmapMono.
+	ret = GetDIBits(hdc, hBitmap, 0,
+		-bitmapInfoMono.bmiHeader.biHeight, // height here needs to be positive. Since we made it negative previously, let's reverse it again to make it positive.
+		&byteBitmapMono,
+		(BITMAPINFO *)&bitmapInfoMono, DIB_RGB_COLORS);
+
+	BYTE byteBitmapMono8bpp[LOGI_LCD_MONO_WIDTH * LOGI_LCD_MONO_HEIGHT];
+
+	// Transform 32bpp data into 8bpp. Let's just take the value of first of 4 bytes (Blue)
+	for (int ii = 0; ii < (LOGI_LCD_MONO_WIDTH * LOGI_LCD_MONO_HEIGHT * 4); ii = ii + 4)
+	{
+		byteBitmapMono8bpp[ii / 4] = byteBitmapMono[ii];
+	}
+
+	LogiLcdMonoSetBackground(byteBitmapMono8bpp);
+
+	// delete the image when done 
+	if (imageMono)
+	{
+		delete imageMono;
+		imageMono = NULL;
+	}
+
+	return true;
+}
+
+bool StrifeLCD::shutdownStrifeLCD()
+{
+	LogiLcdShutdown();
+	return true;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	bool done = false;
+	time_t startTime = time(NULL);
+
+	static StrifeLCD strife;
+
+	strife.initStrifeLCD();
+
+	while ((startTime + 5) > time(NULL)){
+
+	}
+
+	while (!done){
+		strife.updateScreen();
+
+		if ((startTime + 30) < time(NULL))
+		{
+			done = true;
+		}
+	}
+	strife.shutdownStrifeLCD();
+	return 0;
+}
+
---
+++ StrifeLCD.h
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+#pragma comment(lib , "LogitechLcd.lib")
+#include "afxdialogex.h"
+#include "gdiplus.h"
+#include "LogitechLcd.h"
+#include <time.h>
+
+#define STRIFELCD_STAT_GPM				1
+#define STRIFELCD_STAT_GOLD				2
+#define STRIFELCD_STAT_KILLS			3
+#define STRIFELCD_STAT_DEATHS			4
+#define STRIFELCD_STAT_ASSISTS			5
+#define STRIFELCD_STAT_LEVEL			6
+#define STRIFELCD_STAT_CURRENT_HEALTH	7
+#define STRIFELCD_STAT_MAX_HEALTH		8
+#define STRIFELCD_STAT_CURRENT_MANA		9
+#define STRIFELCD_STAT_MAX_MANA			10
+#define STRIFELCD_APP_NAME				L"Strife Profile"
+
+
+class StrifeLCD
+{
+private:
+	const int STARTING_GPM = 0;
+	const int STARTING_GOLD = 0;
+	const int STARTING_KILLS = 0;
+	const int STARTING_DEATHS = 0;
+	const int STARTING_ASSISTS = 0;
+	const int STARTING_LEVEL = 1;
+
+	unsigned long int currentHealth;
+	unsigned long int maxHealth;
+	unsigned long int currentMana;
+	unsigned long int maxMana;
+	unsigned int gold;
+	unsigned int gpm;
+	unsigned int kills;
+	unsigned int deaths;
+	unsigned int assists;
+	unsigned int level;
+
+	unsigned char currentScreen;
+
+	void displayStatsScreen1();
+	void displayStatsScreen2();
+	bool setMonoBackgroundFromFile(TCHAR* fileName);
+
+	void clearDisplay();
+
+public:
+	StrifeLCD();
+
+	//Start the connection to the logitech LCD.
+	bool initStrifeLCD();
+
+	//Disconnect from the logitech LCD screen.
+	bool shutdownStrifeLCD();
+
+	//Show the intro screen.
+	void displaySplashScreen();
+
+	//Update a stat with the given value.
+	void updateStat(unsigned char statType, unsigned long int statValue);
+
+	//Ends the display of the Strife Splash Screen and updates the display screen to display the appropriate stats.
+	void updateScreen();
+};
